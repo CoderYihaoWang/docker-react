2fd260830b5edbc1c50d9c596cdf29cb811d303e
CoderYihaoWang <yesiamyihao@gmail.com>
2021-01-06 00:52:47 +1300

    Fix text on feature branch to pass tests

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Added on the feature branch
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-        Changed on the feature branch
+        learn react
         </a>
       </header>
     </div>
